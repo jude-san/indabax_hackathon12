f370c694713cb069cdce8334755d2d65a26a06c2
komlaRD <ankueric1@gmail.com>
2024-07-10 00:27:53 +0000

    Add navigation bar

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,6 +7,7 @@ import datetime
 
 # Import data from shared.py
 from shared import app_dir, df
+import shiny
 from shiny import reactive
 from shiny.express import input, render, ui
 from functools import partial
@@ -18,13 +19,92 @@ start = datetime.date(2021, 1, 1)
 end = datetime.date(2022, 12, 1)
 
 # Title page
-ui.page_opts(title="Juderic Retail Dashboard", fillable=True)
+# ui.page_opts(title="Juderic Retail Dashboard", fillable=True)
 
 # # Push the navbar items to the right
-# ui.nav_spacer()
+shiny.ui.page_navbar(title="Juderic Retail Dashboard", id="navbar")
+
+with ui.navset_tab(id="home"):
+    with ui.nav_panel("Home"):
+        #  Value boxes
+        with ui.layout_columns(fill=False):
+            with ui.value_box(showcase=icon_svg("coins")):
+                "Unit price"
+
+                @render.text
+                def sum_unit_price():
+                    return filtered_df()["Unit_Price"].sum().round(1)
+
+            with ui.value_box(showcase=icon_svg("scale-balanced")):
+                "Sales Volume"
+
+                @render.text
+                def sum_sales_volume():
+                    return f"{round(filtered_df()['Sales_Volume(KG_LTRS)'].sum(), 1)} kg/L"
+
+            with ui.value_box(showcase=icon_svg("vault")):
+                "Sales Value"
+
+                @render.text
+                def sum_sales_value():
+                    return filtered_df()["Sales_Value"].sum().round(1)
+                
+        with ui.layout_columns(fill=False):
+             with ui.card():
+                "Monthly sales"
+                @render.plot
+                def plot_sales():
+                        # Remove rows with invalid dates
+                    df_sales = filtered_df().loc[:, ["Sales_Value"]]
+                        # Aggregate sales data by month
+                    monthly_sales = df_sales.resample('ME').sum()
+
+                        # Plot the time series data
+                    fig = plt.figure(figsize=(12, 6))
+                    plt.plot(monthly_sales.index,
+                                 monthly_sales['Sales_Value'], marker='o')
+                    plt.title('Monthly Sales Value Over Time')
+                    plt.xlabel('Date')
+                    plt.ylabel('Sales Value')
+                    plt.grid(True)
+                    return fig
+                
+    with ui.nav_panel("Sales forecast"):
+        with ui.card():
+            "Forecast sales"
+            @render.plot
+            def forecast_sales():
+                # Remove rows with invalid dates
+                df_sales = filtered_df().loc[:, ["Sales_Value"]]
+                # Aggregate sales data by month
+                monthly_sales = df_sales.resample('ME').sum()
+                # Build and fit the model
+                model = ExponentialSmoothing(
+                    monthly_sales['Sales_Value'], seasonal='add', seasonal_periods=4).fit()
+
+                # Forecast for the next 12 months
+                forecast = model.forecast(steps=12)
+
+                # Plot the forecast
+                plt.figure(figsize=(12, 6))
+                fig = plt.plot(monthly_sales.index,
+                            monthly_sales['Sales_Value'], marker='o', label='Observed')
+                future_dates = [monthly_sales.index.max() + pd.DateOffset(months=i)
+                                for i in range(1, 13)]
+                forecast_df = pd.DataFrame(
+                    {'Period': future_dates, 'Sales_Value': forecast})
+                plt.plot(forecast_df['Period'], forecast_df['Sales_Value'],
+                        marker='o', linestyle='--', label='Forecasted')
+                plt.title('Sales Value Forecast')
+                plt.xlabel('Date')
+                plt.ylabel('Sales Value')
+                plt.legend()
+                plt.grid(True)
+                return fig
+                
 
 # # Sidebar: Filter controls
-with ui.sidebar(title="Filter controls"):
+with ui.sidebar(title="Filter controls", open="desktop"):
     # Date selector
     ui.input_date_range(
         "date",
@@ -55,80 +135,10 @@ with ui.sidebar(title="Filter controls"):
     # Button
     ui.input_action_button("filter", "Filter")
 
-#  Value boxes
-with ui.layout_columns(fill=False):
-    with ui.value_box(showcase=icon_svg("coins")):
-        "Unit price"
 
-        @render.text
-        def sum_unit_price():
-            return filtered_df()["Unit_Price"].sum().round(1)
-
-    with ui.value_box(showcase=icon_svg("scale-balanced")):
-        "Sales Volume"
-
-        @render.text
-        def sum_sales_volume():
-            return f"{round(filtered_df()['Sales_Volume(KG_LTRS)'].sum(), 1)} kg/L"
-        
-    with ui.value_box(showcase=icon_svg("vault")):
-        "Sales Value"
-
-        @render.text
-        def sum_sales_value():
-            return filtered_df()["Sales_Value"].sum().round(1)
-
-# Plot time series data
-with ui.layout_column_wrap():
-    with ui.card():
-        "Monthly sales"
-        @render.plot
-        def plot_sales():
-            # Remove rows with invalid dates
-            df_sales = filtered_df().loc[:, ["Sales_Value"]]
-            # Aggregate sales data by month
-            monthly_sales = df_sales.resample('ME').sum()
-
-            # Plot the time series data
-            fig = plt.figure(figsize=(12, 6))
-            plt.plot(monthly_sales.index,
-                    monthly_sales['Sales_Value'], marker='o')
-            plt.title('Monthly Sales Value Over Time')
-            plt.xlabel('Date')
-            plt.ylabel('Sales Value')
-            plt.grid(True)
-            return fig
-        
-    with ui.card():
-        "Forecast sales"
-        @render.plot
-        def forecast_sales():
-            # Remove rows with invalid dates
-            df_sales = filtered_df().loc[:, ["Sales_Value"]]
-            # Aggregate sales data by month
-            monthly_sales = df_sales.resample('ME').sum()
-            # Build and fit the model
-            model = ExponentialSmoothing(
-            monthly_sales['Sales_Value'], seasonal='add', seasonal_periods=12).fit()
-
-            # Forecast for the next 12 months
-            forecast = model.forecast(steps=12)
-            
-            # Plot the forecast
-            plt.figure(figsize=(12, 6))
-            fig = plt.plot(monthly_sales.index,
-                    monthly_sales['Sales_Value'], marker='o', label='Observed')
-            future_dates = [monthly_sales.index.max() + pd.DateOffset(months=i)
-                            for i in range(1, 13)]
-            forecast_df = pd.DataFrame({'Period': future_dates, 'Sales_Value': forecast})
-            plt.plot(forecast_df['Period'], forecast_df['Sales_Value'],
-                    marker='o', linestyle='--', label='Forecasted')
-            plt.title('Sales Value Forecast')
-            plt.xlabel('Date')
-            plt.ylabel('Sales Value')
-            plt.legend()
-            plt.grid(True)
-            return fig
+#     # Plot time series data
+# with ui.layout_column_wrap():
+    
               
 # CSS stylesheet
 ui.include_css(app_dir / "styles.css")
@@ -141,9 +151,9 @@ def filtered_df():
     df.set_index("Date", inplace=True)
     start, end = input.date()
     filt_df = df[start:end]
-    filt_df = filt_df[filt_df["City"].isin(input.city())]
-    filt_df = filt_df[filt_df["Channel"].isin(input.channel())]
-    return filt_df
+    df_city = filt_df["City"].isin(input.city())
+    df_channel = filt_df["Channel"].isin(input.channel())
+    return filt_df[df_city | df_channel]
 
 # Embed chatbot
 ui.HTML(
